91417ca127585da9a15b2a5ff02697c5b5ce98b2
Anz Mariya Davis <100871506+anzmariya@users.noreply.github.com>
2023-07-20 22:19:24 +0530

    Add files via upload

---
+++ ip.dart
@@ -0,0 +1,2 @@
+//String server_url = "http://10.0.2.2:5000/";
+String server_url = "http://10.0.2.2:5000/";
---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:child_tracker/screens/parent/locate.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Login(),
+  ));
+}
